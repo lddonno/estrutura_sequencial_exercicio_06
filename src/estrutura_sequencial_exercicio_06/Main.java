b5c561ccc7be0d8d8db9c7f0465311b073561bd0
Leandro de Donno <donno.eng@gmail.com>
2019-10-11 19:01:49 -0300

    Exercicio Resolvido

---
+++ src/estrutura_sequencial_exercicio_06/Main.java
@@ -0,0 +1,36 @@
+package estrutura_sequencial_exercicio_06;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double A, B, C;
+		double triangulo, circulo, trapezio, quadrado, retangulo;
+		
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		C = sc.nextDouble();
+		
+		triangulo = (A * C)/2;
+		circulo = 3.14159 * Math.pow(C, 2);
+		trapezio = ((A + B)/2) * C;
+		quadrado = B * B;
+		retangulo = A * B;
+		
+		System.out.printf("TRIANGULO: %.3f%n", triangulo);
+		System.out.printf("CIRCULO: %.3f%n", circulo);
+		System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+		System.out.printf("QUADRADO: %.3f%n", quadrado);
+		System.out.printf("RETANGULO: %.3f%n", retangulo);
+		
+		
+		sc.close();
+		
+	}
+
+}
